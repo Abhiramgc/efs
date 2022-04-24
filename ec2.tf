977631ed06cb5b0ca43101d43321be89735ea89b
Abhiramgc <36700010+Abhiramgc@users.noreply.github.com>
2022-04-24 07:04:27 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "testinstance" {
-    ami = "ami-033b95fb8079dc481"
+    ami = "ami-04505e74c0741db8d"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.subnet[0].id
     associate_public_ip_address= true
